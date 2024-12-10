b1495907a842f4f473943eeb055809c5e8e514e6
Arkadzio <132197870+Arkadzio@users.noreply.github.com>
2024-12-10 23:18:35 +0100

    basic classes structure set

---
+++ src/main/java/pl/stormit/ideas/model/Question.java
@@ -0,0 +1,22 @@
+package pl.stormit.ideas.model;
+
+import java.util.List;
+
+public class Question {
+
+  private String name;
+  private Category category;
+  private List<Answer> answers;
+
+  public String getName() {
+    return name;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public List<Answer> getAnswers() {
+    return answers;
+  }
+}
