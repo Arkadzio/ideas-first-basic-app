35fff415976a36f6cea98d7ca4111e5ca8799d34
Arkadzio <132197870+Arkadzio@users.noreply.github.com>
2024-12-21 19:04:52 +0100

    files system communication and data saving

---
+++ src/main/java/pl/stormit/ideas/handlers/CategoryCommandHandler.java
@@ -0,0 +1,46 @@
+package pl.stormit.ideas.handlers;
+
+import java.util.List;
+import pl.stormit.ideas.dao.CategoryDao;
+import pl.stormit.ideas.input.UserInputCommand;
+import pl.stormit.ideas.model.Category;
+
+public class CategoryCommandHandler extends BaseCommandHandler {
+
+  private static final String COMMAND_NAME = "category";
+
+  private CategoryDao categoryDao;
+
+  public CategoryCommandHandler() {
+    categoryDao = new CategoryDao();
+  }
+
+  @Override
+  protected String getCommandName() {
+    return COMMAND_NAME;
+  }
+
+  @Override
+  public void handle(UserInputCommand command) {
+
+    switch ((command.getAction())) {
+      case "list":
+        System.out.println("List of categories...");
+        List<Category> categories = categoryDao.findAll();
+        categories.forEach(System.out::println);
+        break;
+
+      case "add":
+        System.out.println("Add category");
+        String categoryName = command.getParam().get(0);
+        categoryDao.add(new Category(categoryName));
+        break;
+
+      default: {
+        throw new IllegalArgumentException(String.format("Unknown action: %s from command: %s", command.getAction(), command.getCommand()));
+
+      }
+    }
+
+  }
+}
